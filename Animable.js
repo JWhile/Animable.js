f7a9c60d46935d330475aec4048939dd7b5af978
JWhile <juloo.dsi@gmail.com>
2014-01-25 00:05:55 +0100

    Don't loop if have 0 animations & remove finished animations

--- Animable.js
+++ Animable.js
@@ -21,28 +21,45 @@ function Animation(from, to, time, update)
 
     this.update = update; // :function
 }
-// function next(long now):void
+// function next(long now):boolean
 Animation.prototype.next = function(now)
 {
     var n = (now - this.start) / this.time * this.diff + this.from;
 
+    if(n > 1)
+    {
+        return true;
+    }
     if(n !== this.last)
     {
         this.last = n;
 
         this.update(n);
     }
+
+    return false;
 };
 
 var animations = []; // :Array<Animation>
 
+// function next():void
 var next = function()
 {
-    newFrame(next);
-
-    for(var i = 0; i < animations.length; ++i)
+    if(animations.length > 0)
     {
-        animations[i].next();
+        newFrame(next);
+
+        var now = Date.now();
+
+        for(var i = 0; i < animations.length; ++i)
+        {
+            if(animations[i].next(now))
+            {
+                animations.splice(i, 1);
+
+                --i;
+            }
+        }
     }
 };
 
