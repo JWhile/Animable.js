747a734e5c9fe1a81ca4afaf51a372bafeb36990
JWhile <juloo.dsi@gmail.com>
2014-01-24 23:51:21 +0100

    Add class Animation

--- Animable.js
+++ Animable.js
@@ -5,3 +5,33 @@
  *
  * Animable.js
  */
+
+(function(){ // namespace
+
+// class Animation
+function Animation(from, to, time, update)
+{
+    this.from = from; // :int
+    this.diff = to - from; // :int
+
+    this.last = from; // :int
+
+    this.start = Date.now(); // :long
+    this.time = time; // :int
+
+    this.update = update; // :function
+}
+// function next(long now):void
+Animation.prototype.next = function(now)
+{
+    var n = (now - this.start) / this.time * this.diff + this.from;
+
+    if(n !== this.last)
+    {
+        this.last = n;
+
+        this.update(n);
+    }
+};
+
+})();
